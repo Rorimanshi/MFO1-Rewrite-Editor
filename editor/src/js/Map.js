b806c2efed027fd6c137c6d2af03aa2cf63e947c
Rorimanshi <kacperswierzak0@gmail.com>
2026-02-19 18:39:37 +0100

    current map info export

--- editor/src/js/Map.js
+++ editor/src/js/Map.js
@@ -58,7 +58,10 @@ class Map {
     }
 
     exportMap(){
-        
+        return {
+            name: 'map name',
+            tiles: this.tiles
+        };
     }
 
     setSize(widthInTiles, heightInTiles){
--- editor/src/js/main.js
+++ editor/src/js/main.js
@@ -1,10 +1,14 @@
 const tileset = new Tileset('tileset', TILE_SIZE);
-tileset.load('../../assets/img/testTileset2.png')
+tileset.load('assets/img/testTileset2.png')
 
 const map = new Map('map', TILE_SIZE);
 map.setSize(20,20);
 map.setTileset(tileset);
 setTimeout(() => {map.setTilesetImg(tileset.getTilesetImg())},1000);
+
+document.getElementById('bntSave')?.addEventListener('click', e => {
+    save();
+});
 
 function mainLoop(){
     tileset.draw()
@@ -12,22 +16,22 @@ function mainLoop(){
 
     requestAnimationFrame(mainLoop);
 }
-test()
-//mainLoop();
+mainLoop();
 
 
-async function test(){
-    const mockData = {
-        smth2: 'test235626',
-        val: '300',
-        o: {t: "tt", h: "h1h1"}
-    };
+async function save(){
+    const mapInfo = map.exportMap();
+    const savefile = {
+        map: mapInfo
+    };  
+    console.log(mapInfo)
+    console.log(savefile)
     const res = await fetch('http://127.0.0.1:27015/save', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(mockData)
+        body: JSON.stringify(savefile)
     });
     const text = await res.text()
     console.log(text);
